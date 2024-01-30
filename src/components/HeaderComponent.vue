24cb08c8c96140d3012cc8e229942891ad78ad37
Laroki <rami.cakir01@gmail.com>
2024-01-30 14:22:18 +0100

    first commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="bg-black">
+    <nav class="grid grid-cols-3 p-6">
+      <div class="flex">
+        <a href="#" class="-m-1.5 p-1.5">
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        </a>
+      </div>
+      <div class="text-white ml-10 grid-cols-2 font-bold">
+        <a class="px-2" href="#">Todos</a>
+        <a class="px-2" href="#">Add Todo</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
